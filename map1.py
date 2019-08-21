85395d64179040e95dd111c792041ef6fe7b81c0
Terry Threatt <terry.threatt@gmail.com>
2019-08-21 14:47:41 -0500

    changed to circle marker

--- map1.py
+++ map1.py
@@ -23,7 +23,7 @@ fg = folium.FeatureGroup(name="My Map")
 
 # location marker loop
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m", fill_color=color_producer(el), color = 'grey', fill=True, fill_opacity=0.7))
 
 # adding child elements
 map.add_child(fg)
