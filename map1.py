36024d0af69bd574ee2cd91fdeb2310763846c7e
Terry Threatt <terry.threatt@gmail.com>
2019-08-21 16:25:42 -0500

    seperated feature layers for layer control

--- map1.py
+++ map1.py
@@ -19,16 +19,25 @@ def color_producer(elevation):
 # map 
 map = folium.Map(location=[29.744814, -95.5873537], zoom_start=6, titles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My Map")
+# feature layer 1
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 # location marker loop
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m", fill_color=color_producer(el), color = 'grey', fill=True, fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m", fill_color=color_producer(el), color = 'grey', fill=True, fill_opacity=0.7))
 
+# feature layer 2
+fgp = folium.FeatureGroup(name="Population")
 # adding child elements
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read()), style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 1000000
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 1000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
 
-map.add_child(fg)
+
+map.add_child(fgv)
+map.add_child(fgp)
+# layer control
+map.add_child(folium.LayerControl())
+
 map.save("Map1.html")
 
