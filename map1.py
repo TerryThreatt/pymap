0ed26169a0b5fb005d9cfe45a73a858ebad5a91e
Terry Threatt <terry.threatt@gmail.com>
2019-08-20 21:58:15 -0500

    added condition elevation colors

--- map1.py
+++ map1.py
@@ -1,14 +1,32 @@
-import folium 
+import folium
+import pandas
 
+# data
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
+# marker color function
+def color_producer(elevation): 
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else: 
+        return 'red'
+
+# map 
 map = folium.Map(location=[29.744814, -95.5873537], zoom_start=6, titles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
+# location marker loop
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
 
-for coordinates in [[38.2, -99.1], [30.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I'm a Marker", icon=folium.Icon(color='green')))
-
+# adding child elements
 map.add_child(fg)
 
 map.save("Map1.html")
+
